e7b940e367cf802888be21af41ea0c5f46bd7f5b
ArianSR29 <green-m-an@hotmail.com>
2018-05-10 23:44:43 -0500

    Nuevos cambios

--- index.php
+++ index.php
@@ -1,23 +1,74 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Portafolio</title>
-</head>
-<body>
-    <div class="c-main-banner">
-        <div class="c-main-text">
-            <img src="assets/img/icon-header.svg" alt="">
-            <div class="c-title">
-                <h4 class="latoB fs54 white"><span>WORKING</span> WITH DESING LAB</h4>
-            </div>
-            <div class="c-desc">
-                <spa n class="latoR fs15 white">We’ve been helping startups launch and grow their businesses. We help you to fill the gap between development, design and product management.</span>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Portafolio</title>
+    </head>
+    <body>
+        <div class="c-main-banner">
+            <div class="c-main-text">
+                <img src="assets/img/icon-header.svg" alt="">
+                <div class="c-title">
+                    <h4 class="latoB fs54 white">
+                        <span>WORKING</span>
+                        WITH DESING LAB</h4>
+                </div>
+                <div class="c-desc">
+                    <span class="latoR fs15 white">We’ve been helping startups launch and grow their
+                        businesses. We help you to fill the gap between development, design and product
+                        management.</span>
+                </div>
             </div>
         </div>
-    </div>
-</body>
+        <div class="c-about-main">
+            <div class="about-text">
+                <div class="about-title">
+                    <span class="latoB fs34 gray3c">ABOUT US</span></div>
+                <div class="">
+                    <span class="latoR fs16 gray3c">
+                        When you work with us, you are part of a team of committed collaborators. All
+                        aspects of our software development process are integrated, iterative and agile.
+                    </span>
+                </div>
+                <div>
+                    <span class="latoR fs16 gray3c">
+                        Our approach unifies design, development and product management to create
+                        exceptional products. We start each project by understanding your business
+                        goals, the user’s needs and the product requirements. Then we help translate
+                        them into effective and elegant solutions.
+                    </span>
+                </div>   
+            </div>
+        </div>
+        <!-- Acá va el Services -->
+
+
+        <!-- Acá  va el what we do-->
+        <div class="c-grilla-main">
+            <div class="grilla-item">
+                    <img src="assets/img/joint.jpg" alt="">
+                    <div class="mask">
+                        <span class="latoB fs34 white">JOINT</span>
+                    </div>
+            </div>
+            <div class="grilla-item"><img src="assets/img/joint.jpg" alt="">
+            <div class="mask">
+                        <span class="latoB fs34 white">JOINT2</span>
+                    </div></div>
+            <div class="grilla-item"><img src="assets/img/joint.jpg" alt=""></div>
+            <div class="grilla-item"><img src="assets/img/joint.jpg" alt=""></div>
+            <div class="grilla-item"><img src="assets/img/joint.jpg" alt=""></div>
+            <div class="grilla-item"><img src="assets/img/joint.jpg" alt=""></div>
+        </div>
+
+
+
+
+
+
+        <!-- acá va el contact us -->
+    </body>
 </html>
