0bcf870773b344f795423201c1b9c1a3aed4b8dc
ArianSR29 <green-m-an@hotmail.com>
2018-05-10 22:29:03 -0500

    banner

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Portafolio</title>
 </head>
 <body>
-    
+    <div class="c-main-banner">
+        <div class="c-main-text">
+            <img src="assets/img/icon-header.svg" alt="">
+            <div class="c-title">
+                <h4 class="latoB fs54 white"><span>WORKING</span> WITH DESING LAB</h4>
+            </div>
+            <div class="c-desc">
+                <spa n class="latoR fs15 white">We’ve been helping startups launch and grow their businesses. We help you to fill the gap between development, design and product management.</span>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
